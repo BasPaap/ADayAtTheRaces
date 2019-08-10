74efc749fe42831140ba70e1801f2eb6e9514a29
Bas Paap <baspaap@live.nl>
2019-08-10 07:29:00 +0200

    Add filesystemwatcher.

--- Bas.ADayAtTheRaces.ControlPanel/Services/DataService.cs
+++ Bas.ADayAtTheRaces.ControlPanel/Services/DataService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,21 @@ namespace Bas.ADayAtTheRaces.ControlPanel.Services
     public sealed class DataService : IDataService
     {
         public event EventHandler Updated;
+        private readonly FileSystemWatcher fileSystemWatcher = new FileSystemWatcher();
+
+        public DataService()
+        {
+            this.fileSystemWatcher.Path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "A Day At The Races");
+            this.fileSystemWatcher.Filter = Environment.ExpandEnvironmentVariables(Properties.Settings.Default.RaceResultsFilePath);
+            this.fileSystemWatcher.NotifyFilter = NotifyFilters.LastWrite;
+            this.fileSystemWatcher.Changed += FileSystemWatcher_Changed;
+            this.fileSystemWatcher.EnableRaisingEvents = true;
+        }
+
+        private void FileSystemWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            //Updated?.Invoke(this, EventArgs.Empty);
+        }
 
         public Collection<RaceResult> GetRaceResults()
         {
